bdc2ab70769f3a9d0f04c547e7ebd1d11743ddc4
zkrakus <zkrakus@gmail.com>
2025-07-30 16:39:20 -0400

    chore:Got eslint working.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,33 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
+  ignorePatterns: ['dist'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: { version: '18.2' }
+  },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'react/prop-types': 'off',
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/prop-types': 'off'
   },
+  overrides: [
+    {
+      files: ['.eslintrc.cjs'],
+      env: { node: true },
+      parserOptions: { sourceType: 'script' }
+    }
+  ]
 }
