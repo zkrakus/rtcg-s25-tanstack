6012693337435efcb8da180f9459162a2f953dda
zkrakus <zkrakus@gmail.com>
2025-08-06 12:36:00 -0400

    feat:rtcg-s25-l420 Prevented refetching of invalidated queries on same page.

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -19,6 +19,7 @@ export default function EventDetails() {
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["events"],
+        refetchType: 'none'
       });
       navigate("/events");
     },
