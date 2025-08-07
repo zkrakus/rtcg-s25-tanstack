58643e6a39d805d6939f4fb5c0f44d425a365ffd
zkrakus <zkrakus@gmail.com>
2025-08-07 13:43:48 -0400

    feat:Section compete. Refactored edit event page to use react router actions and loaders. lost some tanstackquery features. lost optimistic updates. added stale time so tanstack doesn't refetch on page load even though data isn't stale.

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -1,43 +1,56 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  Link,
+  redirect,
+  useNavigate,
+  useParams,
+  useSubmit,
+  useNavigation,
+} from "react-router-dom";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { fetchEvent, queryClient, updateEvent } from "../../util/http.js";
-import LoadingIndicator from "../UI/LoadingIndicator.jsx";
+// import LoadingIndicator from "../UI/LoadingIndicator.jsx";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EditEvent() {
+  const navigate = useNavigate();
+  const { state } = useNavigation();
   const { id } = useParams();
+  const submit = useSubmit();
 
-  const { data, isPending, isError, error } = useQuery({
+  // data will now prepopulate because of the loader
+  const { data, isError, error } = useQuery({
     queryKey: ["events", { id }],
     queryFn: ({ siganl }) => fetchEvent({ siganl, id }),
+    staleTime: 10 * 1000
   });
 
-  const { mutate } = useMutation({
-    mutationFn: updateEvent,
-    onMutate: async ({ event }) => {
-      await queryClient.cancelQueries({ queryKey: ["events", id] }); // Cancels active queries against this key since they are about to be invalidated.
-      const previousEvent = queryClient.getQueryData(["events", id]);
+  // No need to mutate if using a submit action, but lose other benefits.
+  // const { mutate } = useMutation({
+  //   mutationFn: updateEvent,
+  //   onMutate: async ({ event }) => {
+  //     await queryClient.cancelQueries({ queryKey: ["events", id] }); // Cancels active queries against this key since they are about to be invalidated.
+  //     const previousEvent = queryClient.getQueryData(["events", id]);
 
-      queryClient.setQueryData(["events", id], event); // optimistic update.
+  //     queryClient.setQueryData(["events", id], event); // optimistic update.
 
-      return { previousEvent }; // set context with previous event for rollback if necessary.
-    },
-    onError: (error, data, context) => {
-      queryClient.setQueryData(["events", id], context.previousEvent); // Rolls back optimistic update.
-    },
-    onSettled: () => { // Called whenever mutation is done whether successful or not.
-      queryClient.invalidateQueries(["events", id]); // Invalidate any components that are using this key. // Ensures other components sync if update is successful or not.
-    }
-  });
-
-  const navigate = useNavigate();
+  //     return { previousEvent }; // set context with previous event for rollback if necessary.
+  //   },
+  //   onError: (error, data, context) => {
+  //     queryClient.setQueryData(["events", id], context.previousEvent); // Rolls back optimistic update.
+  //   },
+  //   onSettled: () => {
+  //     // Called whenever mutation is done whether successful or not.
+  //     queryClient.invalidateQueries(["events", id]); // Invalidate any components that are using this key. // Ensures other components sync if update is successful or not.
+  //   },
+  // });
 
   function handleSubmit(formData) {
-    mutate({ id, event: formData });
-    navigate("../");
+    // mutate({ id, event: formData });
+    // navigate("../");
+    submit(formData, { method: "PUT" });
   }
 
   function handleClose() {
@@ -46,13 +59,14 @@ export default function EditEvent() {
 
   let content;
 
-  if (isPending) {
-    content = (
-      <div className="center">
-        <LoadingIndicator />
-      </div>
-    );
-  }
+  // Will no longer be pending because we will start with data from the loader.
+  // if (isPending) {
+  //   content = (
+  //     <div className="center">
+  //       <LoadingIndicator />
+  //     </div>
+  //   );
+  // }
 
   if (isError) {
     content = (
@@ -76,15 +90,36 @@ export default function EditEvent() {
   if (data) {
     content = (
       <EventForm inputData={data} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
+        {state === "submitting" ? (
+          <p>Sending data...</p>
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Update
+            </button>
+          </>
+        )}
       </EventForm>
     );
   }
 
   return <Modal onClose={handleClose}>{content}</Modal>;
+}
+
+export function loader({ params }) {
+  return queryClient.fetchQuery({
+    queryKey: ["events", params.id],
+    queryFn: ({ siganl }) => fetchEvent({ siganl, id: params.id }),
+  });
+}
+
+export async function action({ request, params }) {
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+  await updateEvent({ id: params.id, event: updatedEventData });
+  queryClient.invalidateQueries(["events"]); // no optimistic updating in this case.
+  return redirect("../");
 }
